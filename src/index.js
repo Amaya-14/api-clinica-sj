b14b4d263cd51e885f7e2051da86e7c8b42123cd
Daniel Amaya <damayalop@gmail.com>
2022-03-28 19:45:30 -0600

    Creando CRUD del módulo citas

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ app.use(require('./routes/personas/post'))
 app.use(require('./routes/personas/put'))
 app.use(require('./routes/personas/delete'))
 
+app.use(require('./routes/citas/get'))
+app.use(require('./routes/citas/post'))
+app.use(require('./routes/citas/put'))
+app.use(require('./routes/citas/delete'))
+
 // Iniciando el servidor
 app.listen(app.get('port'), () => {
   console.log(`Server on port: ${app.get('port')}`)
